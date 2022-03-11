9bb207e8ea0402f0d7edceb81eb0d7cb3378f452
Alex Hallard <zanda.hallard@outlook.com>
2022-03-11 14:22:46 +0000

    Added whitespace

--- coursework.c
+++ coursework.c
@@ -35,9 +35,11 @@ int countcharacters(char *filename) {
 }
 
 int countcodelines(char *filename) {
-  FILE *file = fopen(filename,"r");
   int codelines = 0;
   char line[1024];
+
+  FILE *file = fopen(filename,"r");
+
   while (fgets(line,1024, file)) { // get characters from file while not end of file
     if (line[0] == '/' && line[1] == '/' && line[2] == ' ') {
       continue;
@@ -49,16 +51,21 @@ int countcodelines(char *filename) {
       codelines++;
     }
   }
+
   fclose(file);
   return codelines;
 }
 
 const char * removecomments(char *filename) {
-  FILE *file = fopen(filename,"r");
   char line[1024];
+
+
+  FILE *file = fopen(filename,"r");
   filename[strlen(filename)-1] = '\0';
-  filename = strcat(filename,"o\0");
-  FILE *outfile = fopen(filename,"w");
+
+  char *ofilename = strcat(filename,"o\0");
+  FILE *outfile = fopen(ofilename,"w");
+
   while (fgets(line,1024, file)) {
     if (line[0] == '/' && line[1] == '/' && line[2] == ' ') {
       continue;
@@ -66,17 +73,20 @@ const char * removecomments(char *filename) {
       fprintf(outfile, "%s", line);
     }
   }
+
   fclose(file);
   fclose(outfile);
   return filename;
 }
 
 const char * balancebrackets(char *filename) {
-  FILE *file = fopen(filename,"r");
   int brackets = 0;
   int bracketscurl = 0;
   int bracketssquare = 0;
   char character;
+
+  FILE *file = fopen(filename,"r");
+
   while ((character = fgetc(file)) != EOF) {
     if (character == '{') {
       bracketscurl++;
@@ -93,7 +103,7 @@ const char * balancebrackets(char *filename) {
     }
   }
   fclose(file);
-  //printf("%d %d %d", brackets, bracketscurl, bracketssquare);
+
   if (brackets == 0 && bracketscurl == 0 && bracketssquare == 0) {
     return "balanced";
   } else {
@@ -101,75 +111,32 @@ const char * balancebrackets(char *filename) {
   }
 }
 
-const char * balancebrackets2(char *filename) {
-  FILE *file = fopen(filename,"r");
-  char character;
-  int i = 0;
-  int bracketsnormal = 0;
-  int bracketscurl = 0;
-  int bracketssquare = 0;
-  char *brackets;
-  char bracket;
-  while ((character = fgetc(file)) != EOF) {
-    if (character == '(' && character == ')' && character == '[' && character == ']' && character == '{' && character == '}') {
-      i++;
-      brackets[i] = character;
-    }
-  }
-  while (i != 0) {
-    bracket = brackets[i];
-    i--;
-    if (character == '{') {
-      if (bracketscurl == 0) {
-        return "unbalanced";
-      }
-      bracketscurl--;
-    } else if (character == '[') {
-      if (bracketssquare == 0) {
-        return "unbalanced";
-      }
-      bracketssquare--;
-    } else if (character == '(') {
-      if (bracketsnormal == 0) {
-        return "unbalanced";
-      }
-      bracketsnormal--;
-    } else if (character == ')') {
-      bracketsnormal++;
-    } else if (character == ']') {
-      bracketssquare++;
-    } else if (character == '}') {
-      bracketscurl++;
-    }
-  }
-  fclose(file);
-  return "balanced";
-}
-
 int functioncount (char *filename) {
-  FILE *file = fopen(filename,"r");
   int functions = 0;
   int counter = 0;
   char line[1024];
   char *words[1024];
   char *word;
 
-  while (fgets(line,1024, file)) {
+  FILE *file = fopen(filename,"r");
 
-    word = strtok (line, " ");
+  while (fgets(line,1024, file)) {
+    word = strtok (line, " "); //Inital word split at space
+
     while (word != NULL) {
       words[counter++] = word;
       word = strtok(NULL, " ");
 
     }
-    if (strcmp(words[0], "int") == 0 || strcmp(words[0],"double") == 0|| strcmp(words[0],"void") == 0 && strcmp(words[2],"(") == 0) {
+    
+    if (strcmp(words[0], "int") == 0 || strcmp(words[0],"double") == 0 || strcmp(words[0],"void") == 0 && strcmp(words[2],"(") == 0) {
       functions++;
     }
   }
   return functions;
 }
 
-int help () {
+void help () {
   printf("\
 \n░█████╗░░█████╗░███╗░░██╗░█████╗░██╗░░░░░██╗░░░██╗░██████╗███████╗\
 \n██╔══██╗██╔══██╗████╗░██║██╔══██╗██║░░░░░╚██╗░██╔╝██╔════╝██╔════╝\
@@ -177,6 +144,7 @@ int help () {
 \n██║░░██╗██╔══██║██║╚████║██╔══██║██║░░░░░░░╚██╔╝░░░╚═══██╗██╔══╝░░\
 \n╚█████╔╝██║░░██║██║░╚███║██║░░██║███████╗░░░██║░░░██████╔╝███████╗\
 \n░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚══════╝░░░╚═╝░░░╚═════╝░╚══════╝\n");
+
   printf("Canalyse v1\n");
   printf("Usage: canalyse [options] file\n\n");
   printf("Canalyse is a commandline c source files analysis tool\n");
@@ -188,40 +156,50 @@ int help () {
   printf("\n         [--functioncount] count the number of functions");
   printf("\n         [--all] run all checks");
   printf("\n         [--help] display this message\n");
-
 }
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("Error no arguments provided\n");
     return 1;
-  } else if (strcmp(argv[1], "--help" ) == 0) {
+
+  } else if (strcmp(argv[1], "--help" ) == 0) { //If first argument matches
     help();
+
   } else if (strcmp(argv[1], "--countlines") == 0) {
     printf("Counting lines of file %s\n", argv[2]);
     printf("Lines: %d\n", countlines(argv[2]));
+
   } else if (strcmp(argv[1], "--countcharacters") == 0) {
     printf("Counting characters of file %s\n", argv[2]);
     printf("Characters: %d\n", countcharacters(argv[2]));
+
   } else if (strcmp(argv[1], "--countcodelines") == 0) {
     printf("Counting code lines of file %s\n", argv[2]);
     printf("Code lines: %d\n", countcodelines(argv[2]));
+
   } else if (strcmp(argv[1], "--removecomments") == 0) {
     printf("Removing comments of file %s\n", argv[2]);
     printf("No comment file saved as: %s\n",removecomments(argv[2]));
+
   } else if (strcmp(argv[1], "--balancebrackets") == 0) {
     printf("Checking if brackets are balanced of file %s\n", argv[2]);
-    printf("Brackets are %s\n", balancebrackets2(argv[2]));
+    printf("Brackets are %s\n", balancebrackets(argv[2]));
+
   } else if (strcmp(argv[1], "--functioncount") == 0) {
     printf("Counting functions in file %s\n", argv[2]);
     printf("There are %d functions\n", functioncount(argv[2]));
+
   } else if (strcmp(argv[1], "--all") == 0) {
     printf("Running all checks on file %s\n", argv[2]);
+    printf("There are %d functions\n", functioncount(argv[2]));
     printf("Lines: %d\n", countlines(argv[2]));
     printf("Characters: %d\n", countcharacters(argv[2]));
     printf("Code lines: %d\n", countcodelines(argv[2]));
     printf("No comment file saved as: %s\n",removecomments(argv[2]));
     printf("Brackets are %s\n", balancebrackets(argv[2]));
-    printf("There are %d functions\n", functioncount(argv[2]));
+
+
   } else {
     printf("Error %s is not a valid argument\n", argv[1]);
   }
